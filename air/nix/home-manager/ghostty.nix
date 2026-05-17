fe16e46fc444caa8ff68c356602f65e76906e249
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2026-05-17 14:35:15 -0400

    [change] revert font change

--- air/nix/home-manager/ghostty.nix
+++ air/nix/home-manager/ghostty.nix
@@ -3,10 +3,9 @@
 {
   xdg.configFile."ghostty/config".text = ''
     theme = light:Night Owlish Light,dark:Night Owl
-    font-family = CommitMono Nerd Font Mono
+    font-family = SF Mono
     font-size = 12
     window-padding-x = 8
     window-padding-y = 8
-    adjust-cell-height = 4
   '';
 }
